2b125489d43aec0a873ba8c313edf7667ec6e0ef
Tamara.Michaud <tamara.michaud@smartfocus.com>
2020-03-17 17:29:07 +0000

    add [wip] begin to work on ArrayList Challenge Exercise

---
+++ ArraysListsAutoboxUnbox/src/arrayList/challenge/ArrayListChallenge.java
@@ -0,0 +1,12 @@
+package arrayList.challenge;
+
+// create a program that implements a simple mobile phone with the following capabilities:
+// able to store, modify, remove and query contact names
+// you will want to create a separate class for Contacts (name and phone number)
+// create a MobilePhone (master) class that holds an ArrayList of Contacts
+// add a menu of options for mobilePhone to; add/remove/edit/list/search/quit
+// do all by names. keep classes private where possible
+
+
+public class ArrayListChallenge {
+}
